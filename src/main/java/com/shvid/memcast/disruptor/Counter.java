92c1a832fee8ed3bf7745ea71dc59243988d65a5
Alex Shvid <a@shvid.com>
2013-10-18 21:27:12 -0700

    shared memory and ssd tests

--- src/main/java/com/shvid/memcast/disruptor/Counter.java
+++ src/main/java/com/shvid/memcast/disruptor/Counter.java
@@ -1,5 +1,6 @@
 package com.shvid.memcast.disruptor;
 
+import java.io.File;
 import java.nio.ByteBuffer;
 import java.nio.LongBuffer;
 import java.util.concurrent.BrokenBarrierException;
@@ -13,9 +14,26 @@ import java.util.concurrent.locks.ReentrantLock;
 
 import com.twitter.jsr166e.LongAdder;
 
-public class Counter {
+/**
+ * Shared Memory disk creation on MacOs
+ * 
+ * diskutil erasevolume HFS+ 'SHM_DISK' `hdiutil attach -nomount ram://1024`
+ * 
+ * You will get disk like this /dev/disk4, create a file as minimum 8 bytes
+ * 
+ * echo "12345678" > /Volumes/SHM_DISK/test-file
+ * 
+ * @author ashvid
+ *
+ */
 
+public class Counter {
+	
+	private static String SHM_FILE = "/Volumes/SHM_DISK/test-file";
+	private static String SSD_FILE = "./data/ssd-test-file";
+	
 	private static final long ITER = 500000000;
+	//private static final long ITER = 1000000;
 
 	static CyclicBarrier barrier = new CyclicBarrier(1);
 
@@ -71,11 +89,65 @@ public class Counter {
 
 	};	
 	
+	static MappedFile mappedSHMFile = null;
+	
+	static final Callable<Long> SIMPLE_SHARED_MEM = new Callable<Long>() {
+
+		public Long call() {
+			await();
+			if (mappedSHMFile == null) {
+				return 0L;
+			}
+			long n0 = System.currentTimeMillis();
+			for (long i = 0; i != ITER; ++i) {
+				long current = UnsafeUtil.UNSAFE.getLong(mappedSHMFile.getAddr());
+				UnsafeUtil.UNSAFE.putLong(mappedSHMFile.getAddr(), current + 1);
+			}
+			return System.currentTimeMillis() - n0;
+		}
+
+	};	
+	
+	static MappedFile mappedSSDFile = null;
+	
+	static final Callable<Long> SIMPLE_SSD_MEM = new Callable<Long>() {
+
+		public Long call() {
+			await();
+			if (mappedSSDFile == null) {
+				return 0L;
+			}
+			long n0 = System.currentTimeMillis();
+			for (long i = 0; i != ITER; ++i) {
+				long current = UnsafeUtil.UNSAFE.getLong(mappedSSDFile.getAddr());
+				UnsafeUtil.UNSAFE.putLong(mappedSSDFile.getAddr(), current + 1);
+			}
+			return System.currentTimeMillis() - n0;
+		}
+
+	};	
+
+	
 	static final long offHeapAddress;
 	
 	static {
 		try {
 			offHeapAddress = UnsafeUtil.UNSAFE.allocateMemory(8);
+			
+			if (new File(SHM_FILE).exists()) {
+				mappedSHMFile = new MappedFile(SHM_FILE, 8);
+			}
+			else {
+				System.out.println("Warning: Shared memory file not found " + SHM_FILE);
+			}
+			
+			if (new File(SSD_FILE).exists()) {
+				mappedSSDFile = new MappedFile(SSD_FILE, 8);
+			}
+			else {
+				System.out.println("Warning: SSD file not found " + SSD_FILE);
+			}
+			
 		}
 		catch(Exception e) {
 			throw new Error(e);
@@ -281,6 +353,16 @@ public class Counter {
 					+ (ohn1 / n1) + "x");
 			
 			
+			long smn1 = SIMPLE_SHARED_MEM.call();
+
+			System.out.println("Simple.SHARED_MEM loop " + smn1 + " slower in "
+					+ (smn1 / n1) + "x");
+			
+			long ssdn1 = SIMPLE_SSD_MEM.call();
+
+			System.out.println("Simple.SSD_MEM loop " + ssdn1 + " slower in "
+					+ (ssdn1 / n1) + "x");
+			
 			long shdn1 = SIMPLE_HEAP_DIRECT.call();
 
 			System.out.println("Simple.HEAP.Direct loop " + shdn1 + " slower in "
@@ -296,33 +378,33 @@ public class Counter {
 			long vn1 = VOLATILE.call();
 
 			System.out.println("Volatile loop " + vn1 + " slower in "
-					+ (vn1 / n1) + "x");
+					+ (vn1 / n1) + "x - concurrent");
 
 			
 			long un1 = UNSAFE_CAS.call();
 
 			System.out.println("HEAP.Unsafe.CAS loop " + un1 + " slower in "
-					+ (un1 / n1) + "x");
+					+ (un1 / n1) + "x - concurrent");
 			
 			long an1 = ATOMIC_LONG.call();
 
 			System.out.println("AtomicLong loop " + an1 + " slower in "
-					+ (an1 / n1) + "x");
+					+ (an1 / n1) + "x - concurrent");
 
 			long lan1 = LONG_ADDER.call();
 
 			System.out.println("LongAdder loop " + lan1 + " slower in "
-					+ (lan1 / n1) + "x");
+					+ (lan1 / n1) + "x - concurrent");
 			
 			long rn1 = REENTRANT_LOCK.call();
 
 			System.out.println("ReentrantLock loop " + rn1 + " slower in "
-					+ (rn1 / n1) + "x");
+					+ (rn1 / n1) + "x - concurrent");
 
 			long sn1 = SYNCHRONIZED.call();
 
 			System.out.println("Synchronized loop " + sn1 + " slower in "
-					+ (sn1 / n1) + "x");
+					+ (sn1 / n1) + "x - concurrent");
 
 						
 			System.out.println("Two Threads long counter from 0 to " + ITER
@@ -335,29 +417,45 @@ public class Counter {
 			Future<Long> f2 = service.submit(SIMPLE);
 			f1.get();
 			n1 = f2.get();
-			System.out.println("Simple loop = " + n1 + " - no consistency");
+			System.out.println("Simple loop = " + n1);
 
 			f1 = service.submit(SIMPLE_HEAP_UNSAFE);
 			f2 = service.submit(SIMPLE_HEAP_UNSAFE);
 			f1.get();
 			shn1 = f2.get();
 			System.out.println("Simple.HEAP.Unsafe loop " + shn1 + " slower in "
-					+ (shn1 / n1) + "x - no consistency");
+					+ (shn1 / n1) + "x");
 			
 			f1 = service.submit(SIMPLE_OFFHEAP_UNSAFE);
 			f2 = service.submit(SIMPLE_OFFHEAP_UNSAFE);
 			f1.get();
 			ohn1 = f2.get();
 			System.out.println("Simple.OFFHEAP.Unsafe loop " + ohn1 + " slower in "
-					+ (ohn1 / n1) + "x - no consistency");
+					+ (ohn1 / n1) + "x");
 
+			
+			f1 = service.submit(SIMPLE_SHARED_MEM);
+			f2 = service.submit(SIMPLE_SHARED_MEM);
+			f1.get();
+			smn1 = f2.get();
+			System.out.println("Simple.SHARED_MEM loop " + smn1 + " slower in "
+					+ (smn1 / n1) + "x");
+			
+			
+			f1 = service.submit(SIMPLE_SSD_MEM);
+			f2 = service.submit(SIMPLE_SSD_MEM);
+			f1.get();
+			ssdn1 = f2.get();
+			System.out.println("Simple.SSD_MEM loop " + ssdn1 + " slower in "
+					+ (ssdn1 / n1) + "x");
+			
 			
 			f1 = service.submit(SIMPLE_HEAP_DIRECT);
 			f2 = service.submit(SIMPLE_HEAP_DIRECT);
 			f1.get();
 			shdn1 = f2.get();
 			System.out.println("Simple.HEAP.Direct loop " + shdn1 + " slower in "
-					+ (shdn1 / n1) + "x - no consistency");
+					+ (shdn1 / n1) + "x");
 			
 			
 			f1 = service.submit(SIMPLE_OFFHEAP_DIRECT);
@@ -365,7 +463,7 @@ public class Counter {
 			f1.get();
 			sohdn1 = f2.get();
 			System.out.println("Simple.OFFHEAP.Direct loop " + sohdn1 + " slower in "
-					+ (sohdn1 / n1) + "x - no consistency");
+					+ (sohdn1 / n1) + "x");
 			
 			
 			f1 = service.submit(VOLATILE);
@@ -373,7 +471,7 @@ public class Counter {
 			f1.get();
 			vn1 = f2.get();
 			System.out.println("Volatile loop " + vn1 + " slower in "
-					+ (vn1 / n1) + "x");
+					+ (vn1 / n1) + "x - concurrent");
 			
 			
 			f1 = service.submit(UNSAFE_CAS);
@@ -381,7 +479,7 @@ public class Counter {
 			f1.get();
 			un1 = f2.get();
 			System.out.println("HEAP.Unsafe.CAS loop " + un1 + " slower in "
-					+ (un1 / n1) + "x");
+					+ (un1 / n1) + "x - concurrent");
 			
 			
 			f1 = service.submit(ATOMIC_LONG);
@@ -390,7 +488,7 @@ public class Counter {
 			an1 = f2.get();
 
 			System.out.println("AtomicLong loop " + an1 + " slower in "
-					+ (an1 / n1) + "x");
+					+ (an1 / n1) + "x - concurrent");
 
 			f1 = service.submit(LONG_ADDER);
 			f2 = service.submit(LONG_ADDER);
@@ -398,7 +496,7 @@ public class Counter {
 			lan1 = f2.get();
 
 			System.out.println("LongAdder loop " + lan1 + " slower in "
-					+ (lan1 / n1) + "x");
+					+ (lan1 / n1) + "x - concurrent");
 			
 			f1 = service.submit(REENTRANT_LOCK);
 			f2 = service.submit(REENTRANT_LOCK);
@@ -406,7 +504,7 @@ public class Counter {
 			rn1 = f2.get();
 
 			System.out.println("ReentrantLock loop " + rn1 + " slower in "
-					+ (rn1 / n1) + "x");
+					+ (rn1 / n1) + "x - concurrent");
 
 			
 			f1 = service.submit(SYNCHRONIZED);
@@ -415,9 +513,11 @@ public class Counter {
 			sn1 = f2.get();
 
 			System.out.println("Synchronized loop " + sn1 + " slower in "
-					+ (sn1 / n1) + "x");
+					+ (sn1 / n1) + "x - concurrent");
 			
 			service.shutdown();
+			
+			mappedSHMFile.close();
 
 		} catch (Exception e) {
 			e.printStackTrace();
