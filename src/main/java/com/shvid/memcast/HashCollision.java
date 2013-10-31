ce63e62b2bce47243737bf6ed7527665e222b3bb
Alex Shvid <a@shvid.com>
2013-10-31 13:25:53 -0700

    HashCollision

---
+++ src/main/java/com/shvid/memcast/HashCollision.java
@@ -0,0 +1,30 @@
+package com.shvid.memcast;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashCollision {
+
+	public static void main(String[] args) {
+	    Set integers = new HashSet();
+	    for (int i = 0; i <= 400; i++)
+	        if ((hash(i) & 0x1f) == 0)
+	            integers.add(i);
+	    Set integers2 = new HashSet();
+	    for (int i = 400; i >= 0; i--)
+	        if ((hash(i) & 0x1f) == 0)
+	            integers2.add(i);
+	    System.out.println(integers);
+	    System.out.println(integers2);
+
+	}
+	
+	static int hash(int h) {
+	    // This function ensures that hashCodes that differ only by
+	    // constant multiples at each bit position have a bounded
+	    // number of collisions (approximately 8 at default load factor).
+	    h ^= (h >>> 20) ^ (h >>> 12);
+	    return h ^ (h >>> 7) ^ (h >>> 4);
+	}
+	
+}
